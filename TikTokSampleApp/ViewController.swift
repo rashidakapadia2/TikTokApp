25903be44acbb3440036dd73378e77d7dd49e9fd
RashidaKapadia <rashida.kapadia@cirrius.com>
2024-12-15 14:44:16 +0530

    Initial Commit

---
+++ TikTokSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTokSampleApp
+//
+//  Created by Cirrius on 15/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
